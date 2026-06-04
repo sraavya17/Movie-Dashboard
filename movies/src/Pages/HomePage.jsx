06d04a483aa11ee861441c7248faf87c2e441ae1
sulakshana sraavya <sulakshanasraavya1@gmail.com>
2026-06-04 20:10:50 +0530

    Add basic HomePage

---
+++ movies/src/Pages/HomePage.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import {useState} from 'react';
+import MovieCard from '../components/MovieCard';
+
+function HomePage() {
+    const [search, setSearch] = useState("");
+
+    const movies = [
+        {id:1, title:"The Shawshank Redemption", year:1994}, 
+        {id:2, title:"The Godfather", year:1972}, 
+        {id:3, title:"The Dark Knight", year:2008}
+    ];
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        alert("Searching for: " + search);
+    }
+
+  return (
+    <div className="home-page">
+        <form className="search-form" onSubmit={handleSearch}>
+            <input type="text" 
+            className="search-input" 
+            placeholder="Search movies..." 
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}/>
+        </form>
+        <button type="submit" className="search-btn" onClick={handleSearch}>Search</button>
+        <div className="movie-grid">
+            {movies.map(movie => <MovieCard movie={movie} key={movie.id} />)}
+        </div>
+
+    </div>
+  )
+}
+
+export default HomePage
---
+++ movies/src/components/MovieCard.jsx
@@ -0,0 +1,23 @@
+
+function onFavoriteClick() {
+    alert("Clicked!");
+}
+
+function MovieCard({movie}) {
+    return (
+        <div className="movie-card">
+            <div className="movie-poster">
+                <img src={movie.poster} alt={movie.title} />
+                <div className="movie-overlay">
+                    <button className="favorite-btn" onClick={onFavoriteClick}>❤︎</button>
+                </div>
+            </div>
+            <div className="movie-info">
+                <h3>{movie.title}</h3>
+                <p>{movie.release_date}</p>
+            </div>
+        </div>
+    )
+}
+
+export default MovieCard;
